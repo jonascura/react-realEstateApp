e46f3c6404890f0a4b256e21253338b7fc5d09f3
Jonas Cura <jonasm.cura@gmail.com>
2025-06-03 16:12:06 -0600

    create searchBar

--- src/componants/navbar/SearchBar.jsx
+++ src/componants/navbar/SearchBar.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./searchBar.scss"
 
+const types = ["buy", "rent"];
+
 const SearchBar = () => {
+  const [query, setQuery] = useState({
+    type: "buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0,
+  });
+
+  // used onClick switch highlighted button type to switch button style
+  const switchType = (event) => {
+    setQuery((prev) => ({ ...prev, type: event}));
+  }
+
   return (
-    <div className='searchBar'>SearchBar</div>
+    <div className='searchBar'>
+      <div className='type'>
+        {types.map(type => (
+          <button 
+            key={type} 
+            onClick={() => switchType(type)}
+            className={query.type === type? 'active' : ''}
+          >
+            {type}
+          </button>
+        ))}
+      </div>
+      <form>
+        <input type='text' name='location' placeholder='City Location' />
+        <input type='number' name='minPrice' placeholder='Min Price' />
+        <input type='number' name='maxPrice' min={0} max={10000000} placeholder='Max Price' />
+        <button>
+          <img src='/search.png' alt='' />
+        </button>
+      </form>
+    </div>
   )
 }
 
--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -19,6 +19,7 @@ const HomePage = () => {
           </p>
           
           <SearchBar />
+          
           <div className="boxes">
             <div className="box">
               <h1>16+</h1>
